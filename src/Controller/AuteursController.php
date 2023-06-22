82b9f4dbaf5c7ccc20bd80c8d6b8900db0e1a851
Jonathan PAIN-CHAMMING'S <jonathan.pchs@gmail.com>
2023-06-22 18:02:24 +0200

    Mise en place du controlleur des Auteurs, ajouter les vérifications avant traitement

--- src/Controller/AuteursController.php
+++ src/Controller/AuteursController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AuteurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class AuteursController extends AbstractController
 {
     #[Route('/auteurs', name: 'app_auteurs')]
-    public function index(): Response
+    public function index(AuteurRepository $auteurRepository): Response
     {
         return $this->render('auteurs/index.html.twig', [
             'controller_name' => 'AuteursController',
+            'auteurs' => $auteurRepository->findBy([], ['auteur' => 'asc'])
+        ]);
+    }
+
+    #[Route('/auteur/details/{id}', name: 'app_auteurs_detail')]
+    public function detail(AuteurRepository $auteurRepository, int $id): Response
+    {
+        return $this->render('auteurs/detail.html.twig', [
+            'controller_name' => 'AuteursController',
+            'auteur' => $auteurRepository->find($id)
+        ]);
+    }
+
+    #[Route('/auteur/suppression/{id}', name: 'app_auteurs_suppression')]
+    public function suppression(AuteurRepository $auteurRepository, int $id): Response
+    {
+        $auteur = $auteurRepository->find($id);
+        // Vérifier le résultat avant traitement
+        $auteur_name = $auteur->getAuteur();
+        $auteurRepository->remove($auteur, true);
+        return $this->render('auteurs/index.html.twig', [
+            'controller_name' => 'AuteursController',
+            'auteurs' => $auteurRepository->findBy([], ['auteur' => 'asc']),
+            'message' => $auteur_name . ' a été effacé'
         ]);
     }
 }
